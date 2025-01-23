15c48e7c4153d50c7151c377e13b16f21df1b530
Byron Hernandez <145072285+rokoisloco@users.noreply.github.com>
2025-01-23 15:26:47 -0800

    Patrollinh thr perimeter
    finished the first 1st vid

---
+++ FiniteStatesP7Hernandez/Assets/GameEnvironment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+
+    private static GameEnvironment instance;
+    public List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints {  get { return checkpoints; } }
+
+   public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+
+            return instance;
+        }
+     
+    }
+}
---
+++ FiniteStatesP7Hernandez/Assets/State.cs
@@ -0,0 +1,123 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE
+    {
+        IDLE, PATROL, PURSUE, ATTACK, SLEEP
+    };
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    };
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+    protected NavMeshAgent agent;
+
+    float visDist = 10.0f;
+    float visAngle = 30.0f;
+    float shootDist = 7.0f;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter() {  stage = EVENT.UPDATE; }
+    public virtual void Update() { stage = EVENT.UPDATE; }
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+        return this;
+    }
+
+
+}
+public class Idle : State
+{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        :base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+       if(Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+}
+
+public class Patrol: State
+{
+    int currentIndex = -1;
+
+    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+      currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+    public override void Update()
+    {
+        if(agent.remainingDistance < 1)
+        {
+            if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1)
+                currentIndex = 0;
+            else
+                currentIndex++;
+
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+        }
+        base.Update();
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking");
+        base.Exit();
+    }
+}
