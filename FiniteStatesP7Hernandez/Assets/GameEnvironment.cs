caf82581330c0c9ff426a696d2bbb72fdd77d97f
Byron Hernandez <145072285+rokoisloco@users.noreply.github.com>
2025-01-27 15:25:52 -0800

    cHASING THEPLAYER PART 1
    cHASING THEPLAYER PART 1 ok

--- FiniteStatesP7Hernandez/Assets/GameEnvironment.cs
+++ FiniteStatesP7Hernandez/Assets/GameEnvironment.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public sealed class GameEnvironment
 {
 
@@ -17,6 +17,8 @@ public sealed class GameEnvironment
             {
                 instance = new GameEnvironment();
                 instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
             }
 
             return instance;
